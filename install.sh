995a7d299272a6828788a9daa641949e2939b32b
ngodfraind <nicolas.godfraind@gmail.com>
2015-03-17 11:42:43 +0100

    install script

---
+++ install.sh
@@ -0,0 +1,30 @@
+#!bin/bash
+
+log() { echo -e "$1"; }
+fail() { echo -e "Release process failed" && exit 1; }
+succeed() { echo -e "Release process succeeded" && exit 0; }
+
+composerjson=$1
+
+#trap fail ERR
+log 'Downloading claroline/Claroline archive...'
+    wget https://github.com/claroline/Claroline/archive/master.zip
+    unzip master.zip
+    mv Claroline-master Claroline
+    rm master.zip
+    cd Claroline
+log 'Installing dependencies...'
+    composer require composer/composer dev-master --prefer-source
+    composer require claroline/bundle-recorder "~4.0" --prefer-source
+
+    log "Copying composer $composerjson"
+    cp composer.json.$composerjson composer.json
+    cp app/config/parameters.yml.dist app/config/parameters.yml
+    composer update --no-dev --prefer-source -o
+log 'Adding placeholders...'
+    touch app/cache/.gitkeep
+    touch app/logs/.gitkeep
+
+succeed
+
+
