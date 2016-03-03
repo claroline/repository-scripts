1c279e0f95612247138a4d285f3c723645d916dd
Godfraind <nicolas.godfraind@gmail.com>
2016-03-03 14:21:07 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,25 +5,18 @@ succeed() { echo "Dev install succeeded" && exit 0; }
 
 #trap fail ERR
 log 'Downloading claroline/Claroline archive...'
-    wget https://github.com/claroline/Claroline/archive/5.x.zip
-    unzip master.zip
-    mv Claroline-master Claroline
-    rm master.zip
-    cd Claroline
+    git clone https://github.com/claroline/Claroline.git Claroline-5
+    cd Claroline-5
+    git checkout 5.x
 log 'Installing dependencies...'
-    composer require composer/composer dev-master --prefer-source
-    composer require claroline/bundle-recorder "~5.0" --prefer-source
-
     log "Copying composer min"
-    cp composer.min.json composer.json
+    cp composer-min.json composer.json
     cp app/config/parameters.yml.dist app/config/parameters.yml
     composer update --no-dev --prefer-source -o
-log 'Adding placeholders...'
-    touch app/cache/.gitkeep
-    touch app/logs/.gitkeep
 log 'Removing operations file...'
     rm app/config/operations.xml
 log 'Updating permissons...'
+    # these are some basic permissions. Feel free to change them.
     chmod -R 0777 app/cache
     chmod -R 0777 app/sessions
     chmod 0777 app/config
@@ -34,7 +27,4 @@ log 'Updating permissons...'
     chmod -R 0777 app/logs
     chmod -R 0777 files
     chmod -R 0777 web/uploads
-    #these are some basic permissions. Feel dree to change them.
 succeed
-
-
